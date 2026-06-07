268dea84c6926d33171f8850ba18a4c953ccde7f
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-07 00:31:40 -0400

    Update Agua.java

--- src/taller3/Desarrollo/Dominio/Agua.java
+++ src/taller3/Desarrollo/Dominio/Agua.java
@@ -1,30 +1,38 @@
 package Dominio;
 
 public class Agua extends Hechizo {
-	
 	private int cantidadHeal;
-	private int presionDelAgua;
+	private int presionAgua;
 	
-	public Agua(String nombre, String tipo, int daño,int cantidadHeal,int presionDelAgua ) {
+	public Agua(String nombre, String tipo, int daño, int cantidadHeal, int presionAgua) {
 		super(nombre, tipo, daño);
-		// TODO Auto-generated constructor stub
 		this.cantidadHeal = cantidadHeal;
-		this.presionDelAgua = presionDelAgua;
-		
+		this.presionAgua = presionAgua;
 	}
 
 	public int getCantidadHeal() {
 		return cantidadHeal;
 	}
 
-	public int getPresionDelAgua() {
-		return presionDelAgua;
+	public int getPresionAgua() {
+		return presionAgua;
 	}
 
 	@Override
 	public void calcularPuntuacion() {
-		setPuntuacion((getDaño() + cantidadHeal + presionDelAgua)*2);
-		
+		this.puntuacion = (this.daño + this.cantidadHeal + this.presionAgua)*2;
 	}
+
+	public void setCantidadHeal(int cantidadHeal) {
+		this.cantidadHeal = cantidadHeal;
+	}
+
+	public void setPresionAgua(int presionAgua) {
+		this.presionAgua = presionAgua;
+	}
+	
+	
+
+}
 
 }
