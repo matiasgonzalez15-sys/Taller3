962ab11f2ab32c477a9d4ef5a5bb9a7498d212c3
matiasgonzalez15-sys <matias.gonzalez15@alumnos.ucn.cl>
2026-06-07 00:28:18 -0400

    Update Sistema.java

--- src/taller3/Desarrollo/Logica/Sistema.java
+++ src/taller3/Desarrollo/Logica/Sistema.java
@@ -3,12 +3,24 @@ package Logica;
 
 public interface Sistema {
 
-	void crearMago(String nombre, String listaHechizos) throws Exception;
+	void crearMago(String nombre, String hechizos) throws Exception;
 	void crearHechizo(String[] partes) throws Exception;
+	void agregarMago(String nombre, String listaHechizos) throws Exception;
+	void modificarMago(String nombre, String tipoNuevo) throws Exception;
+	void eliminarMago(String nombre) throws Exception;
+	void agregarHechizo(String hechizo) throws Exception;
+	void modificarHechizoNombre(String nombre, String nombreNuevo) throws Exception;
+	void modificarHechizoDaño(String nombre, int dañoNuevo) throws Exception;
+	void modificarHechizoTipo(String nombre, String tipoNuevo, int extra) throws Exception;
+	void modificarHechizoTipo(String nombre, String tipoNuevo, int extra, int extra2) throws Exception;
+	void eliminarHechizo(String nombre) throws Exception;
 	void mostrarDiezHechizos();
-    void mostrarTresMagos();
-    void mostrarHechizos();
-    void mostrarMagos();
-    void mostrarHechizosConPuntuacion();
-    void mostrarMagosConPuntuacion();
+	void mostrarTresMagos();
+	void mostrarHechizos();
+	void mostrarMagos();
+	void mostrarHechizosPuntuacion();
+	void mostrarMagosPuntuacion();
+	void modificarHechizoMago(String nombre, String listaHechizos) throws Exception;
+	void guardarMagos() throws Exception;
+	void guardarHechizos() throws Exception;
 }
